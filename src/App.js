21515bb3024ec8e8f4e8409f5665360d482ea3ea
ALOKPANDEY05498 <alokpandey2095@gmail.com>
2022-02-25 14:23:39 +0000

    reactCode

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import UserProfile from './Components/UserProfile/index'
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Alok Pandey',
+    role: 'Softwere Enginer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Kriti',
+    role: 'Softwere Enginer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Aakriti',
+    role: 'Softwere Enginer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Atul',
+    role: 'Softwere Enginer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">User List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
